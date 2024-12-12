129e45b01d15063cdc4170e30b8804f58cf07925
foalscosmo <foalscosmo@gmail.com>
2024-12-12 13:03:11 +0400

    EntryPointScripts

---
+++ YourGameNameEntryPoint.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using com.appidea.MiniGamePlatform.CommunicationAPI;
+using Managers;
+using UnityEngine;
+
+public class YourGameNameEntryPoint : BaseMiniGameEntryPoint
+{
+    [SerializeField] private GameObject gamePrefab;
+    protected override Task LoadInternal()
+    {
+        var gameManager = Instantiate(gamePrefab);
+        gameManager.GetComponent<GameManager>().SetEntryPoint(this);
+        return Task.CompletedTask;
+    }
+
+    protected override Task UnloadInternal()
+    {
+        return Task.CompletedTask;
+    }
+}
