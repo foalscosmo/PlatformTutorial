d95bf7bc3f44047f129673436e0171d0446a4f49
foalscosmo <119937107+foalscosmo@users.noreply.github.com>
2024-12-13 14:41:28 +0400

    Update YourGameNameEntryPoint.cs

--- YourGameNameEntryPoint.cs
+++ YourGameNameEntryPoint.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using com.appidea.MiniGamePlatform.CommunicationAPI;
-using Managers;
 using UnityEngine;
 
 public class YourGameNameEntryPoint : BaseMiniGameEntryPoint
